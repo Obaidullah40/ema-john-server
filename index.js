d32648195084da811da18c5a8d28c35b11d9de22
Sheikh Md Obaidullah <obaidullah2p17@gmail.com>
2021-11-02 23:20:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+
+
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k92al.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// console.log(uri);
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("onlineShop");
+        // console.log('database connected successfully')
+        const productsCollection = database.collection("products");
+        const orderCollection = database.collection("orders");
+
+        // GET products API
+        app.get("/products", async (req, res) => {
+            const cursor = productsCollection.find({});
+            const page = req.query.page;
+            const size = parseInt(req.query.size);
+            let products;
+            const count = await cursor.count();
+            if (page) {
+                products = await cursor
+                    .skip(page * size)
+                    .limit(size)
+                    .toArray();
+            } else {
+                products = await cursor.toArray();
+            }
+            // const products = await cursor.limit(10).toArray();
+
+            res.send({
+                count,
+                products,
+            });
+        });
+
+        // post to get data by keys
+        app.post("/products/byKeys", async (req, res) => {
+            const keys = req.body;
+            const query = { key: { $in: keys } };
+            // console.log("hit the post api", keys);
+            const products = await productsCollection.find(query).toArray();
+            res.json(products);
+        });
+
+        // Add Orders API
+        app.post("/orders", async (req, res) => {
+            const order = req.body;
+
+            // console.log("hit the post api", order);
+            const result = await orderCollection.insertOne(order);
+
+            res.json(result);
+        });
+    } finally {
+        // await client.close( );
+    }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Running my Server with ema jon");
+});
+
+app.listen(port, () => {
+    console.log("Running Server on port", port);
+});
